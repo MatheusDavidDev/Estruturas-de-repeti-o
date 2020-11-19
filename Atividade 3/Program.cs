e388847864510c2f9efc8d6118a5d701e9ee4b4e
matheus <matheusviniciusdavid@gmail.com>
2020-11-19 10:08:43 -0300

    Alteracoes feitas

--- Atividade 3/Program.cs
+++ Atividade 3/Program.cs
@@ -6,18 +6,25 @@ namespace Atividade_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("programa");
+            Console.WriteLine("Validacao de dados");
             Console.WriteLine("--------------");
             Console.WriteLine("Digite seu nome completo");
             string nome = Console.ReadLine();
+
+            while(nome == "")
+            {
+                Console.WriteLine("O nome nao pode ser vazio");
+                Console.WriteLine("Digite seu nome novamente");
+                nome = Console.ReadLine();
+            }
 
             Console.WriteLine("--------------");
             Console.WriteLine("Digite sua idade");
             int idade = int.Parse(Console.ReadLine());
 
-            while(idade >150 )
+            while(idade< 0 || idade > 150 )
             {
-                Console.WriteLine("Digite sua correta!");
+                Console.WriteLine("Digite sua idade correta!");
                 idade = int.Parse(Console.ReadLine());                
             }
 
@@ -32,25 +39,48 @@ namespace Atividade_3
                 salario = float.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("--------------");
-            Console.WriteLine("Digite seu estado civil");
-            Console.WriteLine(" *1* para Solteiro(a)");
-            Console.WriteLine(" *2* para Casado(a)");
-            Console.WriteLine(" *3* para Viuvo(a)");
-            Console.WriteLine(" *4* para Divorcido(a)");
-            Console.WriteLine("--------------");
-            int estadoCivil = int.Parse(Console.ReadLine());
+            bool Validacao = true;
 
-            while(estadoCivil < 1 || estadoCivil > 4)
+            do
             {
-                Console.WriteLine("Voce digitou a opcao errada tente novamente e digite:");
-                Console.WriteLine(" *1* para Solteiro(a)");
-                Console.WriteLine(" *2* para Casado(a)");
-                Console.WriteLine(" *3* para Viuvo(a)");
-                Console.WriteLine(" *4* para Divorcido(a)");
                 Console.WriteLine("--------------");
-                estadoCivil = int.Parse(Console.ReadLine());
-            }
+                Console.WriteLine("Digite seu estado civil");
+                Console.WriteLine(" *s* para Solteiro(a)");
+                Console.WriteLine(" *c* para Casado(a)");
+                Console.WriteLine(" *v* para Viuvo(a)");
+                Console.WriteLine(" *d* para Divorcido(a)");
+                Console.WriteLine("--------------");
+                string estadoCivil = Console.ReadLine();
+
+                switch(estadoCivil.ToLower())
+                {
+                    case "s":
+                        Console.WriteLine("Voce selecionou Solteiro");
+                        Validacao = true;
+                    break;
+
+                    case "c":
+                        Console.WriteLine("Voce selecionou Casado");
+                        Validacao = true;
+                    break;
+
+                    case "v":
+                        Console.WriteLine("Voce selecionou Viuvo");
+                        Validacao = true;
+                    break; 
+
+                    case "d":
+                        Console.WriteLine("Voce selecionou Divorciado");
+                        Validacao = true;
+                    break; 
+
+                    default:
+                        Console.WriteLine("Voce deve selecionar uma das opcoes abaixo");
+                        Validacao = false;
+                    break;
+                }
+                
+            } while (Validacao == false);
             
             Console.WriteLine("--------------");
             Console.WriteLine("Seu cadastro foi concluido Sr(a) "+nome);
